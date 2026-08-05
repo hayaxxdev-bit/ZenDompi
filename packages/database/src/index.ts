f26b007eaa5bb34be6cb718cf08bdb07e75de0f2
hayaxxdev-bit <hayaxx.dev@gmail.com>
2026-08-05 19:21:38 +0700

    fix(database): add explicit .js file extensions to relative imports for ESM resolution

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,19 +1,19 @@
 // Client
-export { prisma } from "./client";
+export { prisma } from "./client.js";
 
 // Types dari Prisma
 export * from "@prisma/client";
 
 // Balance
-export { getWalletBalance, getNetWorth, hasSufficientBalance } from "./balance";
+export { getWalletBalance, getNetWorth, hasSufficientBalance } from "./balance.js";
 
 // Transfer
 export {
   transferBetweenWallets,
   TransferError,
-} from "./transfer";
-export type { TransferInput, TransferResult } from "./transfer";
+} from "./transfer.js";
+export type { TransferInput, TransferResult } from "./transfer.js";
 
 // Transaction (Income/Expense)
-export { createTransaction } from "./transaction";
-export type { SingleEntryInput, SingleEntryResult } from "./transaction";
+export { createTransaction } from "./transaction.js";
+export type { SingleEntryInput, SingleEntryResult } from "./transaction.js";
